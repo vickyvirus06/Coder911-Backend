5e092f39f21f014a784f3c047564afe3a7f84632
vickyvirus06 <vickyvirus1995@protonmail.com>
2024-08-24 11:25:38 +0530

    minor change

--- CoderService/src/main/java/com/coder911/coder/repository/CoderRepository.java
+++ CoderService/src/main/java/com/coder911/coder/repository/CoderRepository.java
@@ -1,15 +1,20 @@
 package com.coder911.coder.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.coder911.coder.entity.Coder;
+import com.coder911.coder.entity.CoderId;
 
 @Repository
-public interface CoderRepository extends JpaRepository<Coder, Integer>{
+public interface CoderRepository extends JpaRepository<Coder, CoderId>{
 
-	Coder findByEmailAndPassword(String email, String password);
+	@Query("SELECT c FROM Coder c WHERE c.id.email = :email")
+	Coder findByEmail(@Param("email") String email);
 
-	Coder findByEmail(String email);
+	@Query("SELECT c FROM Coder c WHERE c.id.email = :email AND c.id.password = :password")
+	Coder findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
 
 }
